b189b7ecc97cf274996c1a234b38b9f517b27eed
Muhammad Nur Ramadhani <145341299+MuhRamadhani@users.noreply.github.com>
2025-06-10 23:33:37 +0800

    Add next.config.ts 1749569617108

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '4mb',
+    },
+  },
+}
+
+export default nextConfig
